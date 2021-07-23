30e1b0215680ec373d9e21a49c130583480f82e8
jlfagundes <jlfagundes@gmail.com>
2021-07-23 19:18:18 -0300

    feat: exercise 4 done

---
+++ back-end/bloco-21-functions-sql-joins-e-subqueries/dia-1-functions-mais-usadas-sql/exercise4.sql
@@ -0,0 +1,3 @@
+-- Extraia a palavra "JavaScript" da frase 'A linguagem JavaScript está entre as mais usadas' .
+-- Solução:
+SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 13, 11)
