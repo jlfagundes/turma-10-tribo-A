24ae410a2282cf2944ba08f18471420c32b5c3c8
jlfagundes <jlfagundes@gmail.com>
2021-09-05 18:50:59 -0300

    exercise 3 bloco 26 dia 1

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/my-scripts/index.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/my-scripts/index.js
@@ -1,7 +1,10 @@
-const peso = 85;
-const altura = 175
+const readline = require('readline-sync');
+
+const peso = readline.questionInt('Qual o seu peso? ');
+const altura = readline.questionInt('Qual a sua altura? ');
 
 function calcImc() {
+  
   const imc = (peso / Math.pow(altura / 100, 2)).toFixed(2);
   console.log(imc);
 }
